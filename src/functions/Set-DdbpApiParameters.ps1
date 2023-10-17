098006565492365411793a155478d45eca5b084c
Kent Sapp <66083310+cksapp@users.noreply.github.com>
2023-10-17 19:42:36 -0400

    Update Set-DdbpApiParameters.ps1

--- src/functions/Set-DdbpApiParameters.ps1
+++ src/functions/Set-DdbpApiParameters.ps1
@@ -1,26 +1,26 @@
 function Set-DdbpApiParameters {
-	<#
-	.SYNOPSIS
-	Sets the API Parameters used throughout the module.
+    <#
+    .SYNOPSIS
+    Sets the API Parameters used throughout the module.
 
-	.PARAMETER Url
-	Provide Datto DBPool API Url. See Datto DBPool API help files for more information.
+    .PARAMETER Url
+    Provide Datto DBPool API Url. See Datto DBPool API help files for more information.
 
-	.PARAMETER Key
-	Provide Dattto DBPool API Key. You can find your user API key at [/web/self](https://dbpool.datto.net/web/self).
-	
-	#>
-	
-	Param(
-	[Parameter(Position = 0, Mandatory=$False)]
-	[Uri]$apiUrl = "https://dbpool.datto.net/api/v2",
+    .PARAMETER Key
+    Provide Datto DBPool API Key. You can find your user API key at [/web/self](https://dbpool.datto.net/web/self).
+    #>
     
-	[Parameter(Position = 1, Mandatory=$True)]
-	[string]$apiKey
+    Param(
+        [Parameter(Position = 0, Mandatory=$False)]
+        [ValidateScript({
+            $_ -match '^https://dbpool\.datto\.net/api/v2/?$'
+        })]
+        [Uri]$apiUrl = "https://dbpool.datto.net/api/v2/",
+        
+        [Parameter(Position = 1, Mandatory=$True)]
+        [string]$apiKey
+    )
 
-	)
-
-	New-Variable -Name apiUrl -Value $apiUrl -Scope Script -Force
-	New-Variable -Name apiKey -Value $apiKey -Scope Script -Force
-
+    New-Variable -Name apiUrl -Value $apiUrl -Force
+    New-Variable -Name apiKey -Value $apiKey -Force
 }
