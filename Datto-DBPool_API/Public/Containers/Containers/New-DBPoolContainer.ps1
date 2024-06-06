7bf696c59791ce3ed14b34d9101da1503ffa4f67
Kent Sapp <git@kentsapp.com>
2024-06-06 19:13:26 -0400

    Accept pipeline input for new container based on parent name, ID, or default database. This will allow for pipeing objects to New-DBPoolContainer based on property name

--- Datto-DBPool_API/Public/Containers/Containers/New-DBPoolContainer.ps1
+++ Datto-DBPool_API/Public/Containers/Containers/New-DBPoolContainer.ps1
@@ -1,44 +1,69 @@
 function New-DBPoolContainer {
 <#
     .SYNOPSIS
-        A short one-line action-based description, e.g. 'Tests if a function is valid'
+        The New-DBPoolContainer function is used to create a new container from the DBPool API.
+
     .DESCRIPTION
-        A longer description of the function, its purpose, common use cases, etc.
-    .NOTES
-        Information or caveats about the function e.g. 'This function is not supported in Linux'
-    .LINK
-        Specify a URI to a help page, this will show when Get-Help -Online is used.
+        The New-DBPoolContainer function is used to create a new container in the DBPool based on the provided container name and parent container information.
+
+        This requires a container name and at least one at least one of the parent* fields must be specified.
+        If multiple parent fields are specified, both conditions will have to match a parent for it to be selected.
+
+    .PARAMETER ContainerName
+        The name for the container to create.
+    
+    .PARAMETER ParentId
+        The ID of the parent container to clone.
+    
+    .PARAMETER ParentName
+        The name of the parent container to clone.
+    
+    .PARAMETER ParentDefaultDatabase
+        The default database of the parent container to clone.
+    
     .EXAMPLE
-        Test-MyTestFunction -Verbose
-        Explanation of the function or its result. You can include multiple examples with additional .EXAMPLE lines
+        New-DBPoolContainer -ContainerName 'MyNewContainer' -ParentId 12345
+
+    .EXAMPLE
+        Get-DBPoolContainer -ParentContainer -Id 1 | New-DBPoolContainer -ContainerName 'MyNewContainer'
+
+    .NOTES
+        N/A
+
+    .LINK
+        N/A
 #>
 
     [CmdletBinding()]
     param (
         [Parameter(Mandatory = $true, ValueFromPipeline = $true, ValueFromPipelineByPropertyName = $true)]
-        [string]$Name,
+        [string]$ContainerName,
 
-        [Parameter(Mandatory = $false, ValueFromPipeline = $true, ValueFromPipelineByPropertyName = $true)]
+        [Parameter(Mandatory = $false, ValueFromPipelineByPropertyName = $true)]
         [ValidateRange(1, [int]::MaxValue)]
+        [Alias("Id")]
         [int]$ParentId,
 
-        [Parameter(Mandatory = $false, ValueFromPipeline = $true, ValueFromPipelineByPropertyName = $true)]
+        [Parameter(Mandatory = $false, ValueFromPipelineByPropertyName = $true)]
+        [Alias("Name")]
         [string]$ParentName,
 
-        [Parameter(Mandatory = $false, ValueFromPipeline = $true, ValueFromPipelineByPropertyName = $true)]
+        [Parameter(Mandatory = $false, ValueFromPipelineByPropertyName = $true)]
+        [Alias("DefaultDatabase")]
         [string]$ParentDefaultDatabase
     )
 
     begin {
+        $method = 'POST'
+        $requestPath = "/api/v2/containers"
+        $body = @{}
+    }
+
+    process {
+        $body['name'] = $ContainerName
 
         if (-not ($PSBoundParameters.ContainsKey('ParentId') -or $PSBoundParameters.ContainsKey('ParentName') -or $PSBoundParameters.ContainsKey('ParentDefaultDatabase'))) {
             throw "At least one parent parameter must be provided."
-        }
-
-        $method = 'POST'
-        $requestPath = "/api/v2/containers"
-        $body = @{
-            name   = $Name
         }
 
         if ($PSBoundParameters.ContainsKey('ParentId')) {
@@ -52,13 +77,10 @@ function New-DBPoolContainer {
         if ($PSBoundParameters.ContainsKey('ParentDefaultDatabase')) {
             $body.'parent.defaultDatabase' = $ParentDefaultDatabase
         }
-    }
-
-    process {
 
         Invoke-DBPoolRequest -method $method -resource_Uri $requestPath -data $body
-
     }
     
-    end {}
+    end {
+    }
 }
