0cc72a4245298b3d93d58f29291ca6ae001db8ce
Kent Sapp <git@kentsapp.com>
2024-06-07 14:37:48 -0400

    Change OpenAPI spec path to parameter with default path

--- Datto-DBPool_API/Public/OpenAPI/Get-DBPoolOpenAPI.ps1
+++ Datto-DBPool_API/Public/OpenAPI/Get-DBPoolOpenAPI.ps1
@@ -6,29 +6,37 @@ function Get-DBPoolOpenAPI {
     .DESCRIPTION
         Gets the OpenAPI json spec for the DBPool API documentation.
 
+    .PARAMETER OpenAPI_Path
+        The path to the OpenAPI json spec.
+        This defaults to '/api/docs/openapi.json'
+    
+    .EXAMPLE
+        Get-DBPoolOpenAPI
+
+        This will get the OpenAPI json spec for the DBPool API documentation.
+
     .NOTES
         N/A
 
     .LINK
         N/A
-
-    .EXAMPLE
-        Get-DBPoolOpenAPI
-        This will get the OpenAPI json spec for the DBPool API documentation.
 #>
 
 
     [CmdletBinding()]
     param (
-        
+        [Parameter(Mandatory = $false)]
+        [string]$OpenAPI_Path = '/api/docs/openapi.json'
     )
     
     begin {
-        $requestPath = '/api/docs/openapi.json'
+        $requestPath = $OpenAPI_Path
     }
     
     process {
+
         Invoke-DBPoolRequest -Method Get -resource_Uri $requestPath
+
     }
     
     end {}
