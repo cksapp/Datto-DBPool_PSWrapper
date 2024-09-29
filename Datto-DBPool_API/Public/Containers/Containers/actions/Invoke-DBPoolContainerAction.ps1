eec439f9858a0865da5d0ee605ed6f315b8a0966
Kent Sapp <git@kentsapp.com>
2024-09-29 15:05:48 -0400

    Add parallel processing to Invoke-DBPoolContainerActions (#13)
    * Refeactor Container Actions for PS Jobs parallel tasks
    
    * Refeactor Container Actions for PS Jobs parallel tasks
    
    * Surpress confirmation prompt remove job
    
    * Add 'X-App-Request-Id' global variable from API response header, also adds debug out message. Other misc formatting
    
    * Change PSJobs to runspaces for invoke-container actions for better efficiency
    
    * switch to return warning variables from runspaces
    
    * Update New-DBPoolContainer error and comments
    
    * pipe runspace stream directly to parent session rather than using pscustomobject

--- Datto-DBPool_API/Public/Containers/Containers/actions/Invoke-DBPoolContainerAction.ps1
+++ Datto-DBPool_API/Public/Containers/Containers/actions/Invoke-DBPoolContainerAction.ps1
@@ -15,6 +15,9 @@ function Invoke-DBPoolContainerAction {
         Start, Stop, and Restart are all considered minor actions and will not require a confirmation prompt.
         Refresh and Schema-Merge are considered major actions and will require a confirmation prompt.
 
+    .PARAMETER Force
+        Skip the confirmation prompt for major actions, such as 'Refresh' and 'Schema-Merge'.
+
     .EXAMPLE
         Invoke-DBPoolContainerAction -Id '12345' -Action 'restart'
 
@@ -57,25 +60,38 @@ function Invoke-DBPoolContainerAction {
 
         [Parameter(Mandatory = $true, Position = 1)]
         [ValidateSet('refresh', 'schema-merge', 'start', 'restart', 'stop', IgnoreCase = $false)]
-        [string]$Action
+        [string]$Action,
+
+        [switch]$Force
     )
 
     begin {
 
         $method = 'POST'
 
-        # Write warning when using deprecated 'schema-merge' action
+        # Write warning when using deprecated 'schema-merge' action, otherwise set confirmation prompt for 'major' actions
         if ($Action -eq 'schema-merge') {
-            Write-Warning "The action [ schema-merge ] is deprecated! Use the [ refresh ] action as the supported way to update a container."
+            Write-Warning 'The action [ schema-merge ] is deprecated! Use the [ refresh ] action as the supported way to update a container.'
+            $ConfirmPreference = 'Medium'
+        } elseif ($Action -eq 'refresh' -and -not $Force) {
             $ConfirmPreference = 'Medium'
         }
+
+        $moduleName = $MyInvocation.MyCommand.Module.Name
+        if ([string]::IsNullOrEmpty($moduleName)) {
+            Write-Error 'The function is not loaded as part of a module or the module name is not available.' -ErrorAction Stop
+        }
+        $modulePath = (Get-Module -Name $moduleName).Path
+
+        $runspacePool = [runspacefactory]::CreateRunspacePool(1, [Environment]::ProcessorCount - 1)
+        $runspacePool.Open()
+        $runspaces = [System.Collections.ArrayList]::new()
 
     }
 
     process {
 
         foreach ($n in $Id) {
-            $requestResponse = $null
             $requestPath = "/api/v2/containers/$n/actions/$Action"
 
             # Try to get the container name to output for the ID when using the Verbose preference
@@ -88,23 +104,58 @@ function Invoke-DBPoolContainerAction {
                 }
             }
 
-            if ($PSCmdlet.ShouldProcess("Container [ ID: $n ]", "[ $Action ]")) {
-                Write-Verbose "Peforming action [ $Action ] on Container [ ID: $n, Name: $containerName ]"
+            if ($Force -or $PSCmdlet.ShouldProcess("Container [ ID: $n ]", "[ $Action ]")) {
+                Write-Verbose "Performing action [ $Action ] on Container [ ID: $n, Name: $containerName ]"
 
-                try {
-                    $requestResponse = Invoke-DBPoolRequest -method $method -resource_Uri $requestPath -ErrorAction Stop
-                }
-                catch {
-                    Write-Error $_
-                }
+                $runspace = [powershell]::Create().AddScript({
+                        param ($method, $requestPath, $modulePath, $baseUri, $apiKey, $action, $containerId)
 
-                if ($requestResponse.StatusCode -eq 204) {
-                        Write-Output "Success: Invoking Action [ $Action ] on Container [ ID: $n ]."
-                    }
+                        Import-Module $modulePath
+                        Add-DBPoolBaseURI -base_uri $baseUri
+                        Add-DBPoolApiKey -apiKey $apiKey
+
+                        $warningPreference = 'Continue'
+
+                        try {
+                            $requestResponse = Invoke-DBPoolRequest -method $method -resource_Uri $requestPath -WarningVariable warnings -ErrorAction Stop
+                            if ($requestResponse.StatusCode -eq 204) {
+                                Write-Output "Success: Invoking Action [ $action ] on Container [ ID: $containerId ]."
+                            }
+                        } catch {
+                            Write-Error "Error: $($_.Exception.Message)"
+                        }
+                    }).AddArgument($method).AddArgument($requestPath).AddArgument($modulePath).AddArgument($Global:DBPool_Base_URI).AddArgument($Global:DBPool_ApiKey).AddArgument($Action).AddArgument($n)
+
+                $runspace.RunspacePool = $runspacePool
+                $runspaceData = [pscustomobject]@{ Pipe = $runspace; ContainerId = $n; Handle = $runspace.BeginInvoke() }
+                $runspaces.Add($runspaceData) | Out-Null
             }
         }
 
+        while ($runspaces.Count -gt 0) {
+            $completedRunspaces = $runspaces | Where-Object { $_.Handle.IsCompleted }
+            foreach ($runspace in $completedRunspaces) {
+                $runspace.Pipe.EndInvoke($runspace.Handle)
+
+                $runspace.Pipe.Streams | ForEach-Object {
+                    $_.Output | ForEach-Object { Write-Output $_ }
+                    $_.Warning | ForEach-Object { Write-Warning $_.Message }
+                    $_.Error | ForEach-Object { Write-Error $_.Exception.Message }
+                }
+
+                $runspace.Pipe.Dispose()
+                $runspaces.Remove($runspace) | Out-Null
+            }
+
+            Start-Sleep -Milliseconds 500
+        }
+
     }
-    
-    end {}
+
+    end {
+
+        $runspacePool.Close()
+        $runspacePool.Dispose()
+
+    }
 }
